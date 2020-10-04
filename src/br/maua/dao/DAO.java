2dba4e9858ea776f19f15d8178de8bfa32199966
Leonardo <leonardo_r@fastmail.com>
2020-10-04 16:44:59 -0300

    Finalizando DAO para Anime

--- src/br/maua/dao/DAO.java
+++ src/br/maua/dao/DAO.java
@@ -1,5 +1,8 @@
 package br.maua.dao;
 
+import br.maua.exceptions.NoEntryDB;
+
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -14,8 +17,11 @@ public interface DAO <T> {
 
     String DATABASE = "medias.db";
 
+
+
     /**
      * Lista todos os itens cadastrados no banco de dados
+     * @return lista com todos os itens da categoria
      */
 
     List<T> getAll();
@@ -25,26 +31,26 @@ public interface DAO <T> {
      * @param t parametro genérico para o objeto de procura no banco de dados
      */
 
-    void insertEntry(T t);
+    void insertEntry(T t) throws SQLException;
 
     /**
      * remove objeto de midia no banco de dados local
      * @param t parametro genérico para o objeto de procura no banco de dados
      */
 
-    void deleteEntry(T t);
+    void deleteEntry(T t) throws SQLException;
 
     /**
      * recupera o objeto cadastrado no banco de dados por seu ID
      * @param ID número que representa a identidade de cadastro do objeto
      */
 
-    T getEntryID(int ID);
+    T getEntryID(int ID) throws NoEntryDB;
 
     /**
      * recupera o objeto cadastrado no banco de dados por seu título
      * @param title nome do objeto cadastrado
      */
 
-    T getEntryTitle(String title);
+    T getEntryTitle(String title) throws NoEntryDB;
 }
