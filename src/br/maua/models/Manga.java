758cf9007eccf6bfe6584cbde2edbb666275a40f
Leonardo <leonardo_r@fastmail.com>
2020-10-04 12:54:49 -0300

    implementando modelos de midia II, refatorando estrutura

--- src/br/maua/models/Manga.java
+++ src/br/maua/models/Manga.java
@@ -1,4 +1,20 @@
 package br.maua.models;
 
-public class Manga {
+public class Manga extends MediaType {
+
+    private final String type;
+    private final int chapters;
+    private final int volumes;
+
+
+    Manga(int id, String title, String description,String type, int chapters, int volumes, double score, String poster_url){
+
+        super(id, title, description, score, poster_url);
+
+        this.type = type;
+        this.chapters = chapters;
+        this.volumes = volumes;
+
+    };
+
 }
