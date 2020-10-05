603641080ab906097c25f65705b440aab030650f
Leonardo <>
2020-10-05 08:58:12 -0300

    app doc

--- src/br/maua/interfaces/App.java
+++ src/br/maua/interfaces/App.java
@@ -15,20 +15,31 @@ import java.util.Scanner;
 
 public class App {
 
-    public static AnimeDAO animeDAO = new AnimeDAO();
-    public static MangaDAO mangaDAO = new MangaDAO();
+    private static AnimeDAO animeDAO = new AnimeDAO();
+    private static MangaDAO mangaDAO = new MangaDAO();
 
+    /**
+     * Displays search results formatted with ID for user to choose
+     * @param results List of objects implementing IMedia (with title and id)
+     */
     public static void printResults(ArrayList<IMedia> results){
         results.forEach(res -> {
             System.out.println("[ID: "+res.getId()+"] - " + res.getTitle());
         });
     }
 
+    /**
+     * Prints goodbye and ends the program with a success code
+     */
     private static final AvailableAction Quit = ()->{
         System.out.println("Goodbye");
         System.exit(0);
     };
 
+    /**
+     * Prompts user for anime title, searches the local database and if nothing is found searches the API.
+     * Lists the result and prompts user for the anime's ID. Then prints the result, and adds the anime to the DB if it wasn't already present.
+     */
     private static final AvailableAction SearchAnime = ()->{
         boolean inDatabase = true;
         ArrayList<IMedia> searchResults = new ArrayList<>();
@@ -59,6 +70,10 @@ public class App {
         System.out.println("ANIME: " + anime);
     };
 
+    /**
+     * Prompts user for manga title, searches the local database and if nothing is found searches the API.
+     * Lists the result and prompts user for the anime's ID. Then prints the result, and adds the manga to the DB if it wasn't already present.
+     */
     private static final AvailableAction SearchManga = ()->{
         boolean inDatabase = true;
         ArrayList<IMedia> searchResults = new ArrayList<>();
@@ -89,12 +104,22 @@ public class App {
         System.out.println(manga);
     };
 
+    /**
+     * Displays all media currently in the database
+     */
     private static final AvailableAction ViewDB = ()->{
         System.out.println("Look at the database!!!");
     };
 
+    /**
+     * Array of arrow functions (AvailableMedia) with indices corresponding to the menu's listing
+     */
     private static final AvailableAction[] actions = {Quit, SearchAnime, SearchManga, ViewDB};
 
+    /**
+     * Main method for the application
+     * Displays the title, and prompts user for one of the available action until the user quits the program
+     */
     public static void Start() {
         Menu.title();
         while(true){
